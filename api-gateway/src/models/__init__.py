912cb8a48ec36d96c4982c834dc60c0b0da889fb
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-05-23 21:45:42 +0200

    Create __init__.py

---
+++ api-gateway/src/models/__init__.py
@@ -0,0 +1,17 @@
+"""
+Models Module - Schemas e contratos de dados
+"""
+
+from .schemas import (
+    VerifyRequest,
+    VerifyResponse,
+    DadosPublicos,
+    VerifyErrorResponse,
+)
+
+__all__ = [
+    "VerifyRequest",
+    "VerifyResponse",
+    "DadosPublicos",
+    "VerifyErrorResponse",
+]
