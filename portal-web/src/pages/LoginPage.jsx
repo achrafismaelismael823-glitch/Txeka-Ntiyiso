ae727e6238d376e8f8c19fba6babb590ace6d4ac
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-05-24 07:31:52 +0200

    Create LoginPage.jsx

---
+++ portal-web/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/LoginPage.jsx
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <LoginForm />
+    </div>
+  );
+}
