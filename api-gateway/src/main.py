3c5b63587963190d89110b61963bf565584d7fc2
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-05-30 21:37:47 +0200

    Update main.py

--- api-gateway/src/main.py
+++ api-gateway/src/main.py
@@ -2,7 +2,7 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routes import verify
-
+from src.routes import emit
 # Configuração de Logs Institucionais
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("src.main")
@@ -34,8 +34,9 @@ def root_health_check():
 
 # Ativação das Rotas do Ecossistema
 app.include_router(verify.router, prefix="/api/v1")
+app.include_router(emit.router)
 logger.info("Registando rotas sob prefixo: /api/v1")
 
 @app.on_event("startup")
 def startup_event():
-    logger.info("🚀 Txeka Ntiyiso API Gateway operacional em produção...")
+    logger.info("Txeka Ntiyiso API Gateway operacional em produção...")
