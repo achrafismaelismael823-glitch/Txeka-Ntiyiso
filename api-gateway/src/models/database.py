7afa96ef6aeaf5773caa7114b388cb014c87209a
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-06-04 14:35:32 +0200

    Ensure DATABASE_URL is defined in environment variables
    Add environment variable check for DATABASE_URL

--- api-gateway/src/models/database.py
+++ api-gateway/src/models/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.models import Base
 
+import os
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("DATABASE_URL não está definida. Verifica as Environment Variables no Render")
