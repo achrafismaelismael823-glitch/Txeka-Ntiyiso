265f792fdb5720592338109180c34177bf870c98
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-06-11 01:23:19 +0200

    Update database.py

--- api-gateway/src/models/database.py
+++ api-gateway/src/models/database.py
@@ -1,30 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from src.models import Base
-import asyncpg  # eliminar depois dos taste
-import os
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("DATABASE_URL não está definida. Verifica as Environment Variables no Render")
+from datetime import datetime, timezone
+from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import declarative_base
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL) 
+Base = declarative_base()
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-db = None  # pool global pra usar no /dev/create-demo
-
-async def init_db():
-    global db
-    db = await asyncpg.create_pool(
-        dsn=SQLALCHEMY_DATABASE_URL,
-        ssl="require"  # Supabase pooler precisa disso
+class AuditBase(Base):
+    __abstract__ = True
+    created_at = Column(
+        DateTime(timezone=True), 
+        default=lambda: datetime.now(timezone.utc), 
+        nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True), 
+        default=lambda: datetime.now(timezone.utc), 
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False
     )
