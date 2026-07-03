89fe2c6279e83d0fef9bc352a992d416be5b71c9
achrafismaelismael823-glitch <achrafismaelismael823@gmail.com>
2026-07-03 16:05:18 +0000

    fix(main): adiciona institution_routes ao import
    - Faltava importar institution_routes no main.py
    - Causava NameError no deploy
    
    🇲🇿 Txeka Ntiyiso

--- api-gateway/src/main.py
+++ api-gateway/src/main.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 from src.database import init_db
-from src.routes import emission_routes, verify, revocation, audit_routes
+from src.routes import emission_routes, verify, revocation, audit_routes, institution_routes
 from src.exceptions import TxekaNtiyisoException, txeka_exception_handler
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
